bed84526c01ff07464ffd4438ad2b104b5bbb54b
jeetpaul1 <pauljeet559@gmail.com>
2024-12-12 23:13:31 +0530

    type conversion

---
+++ Python-for-DSA/Day-2/type_conversion.py
@@ -0,0 +1,15 @@
+#Write a program to convert a string to an integer, an integer to a float, and a float to a string.
+
+
+str_num = "123" #solution
+int_num = 456
+float_num = 78.9
+
+# Conversions
+str_to_int = int(str_num)
+int_to_float = float(int_num)
+float_to_str = str(float_num)
+
+print("String to Integer:", str_to_int)
+print("Integer to Float:", int_to_float)
+print("Float to String:", float_to_str)
