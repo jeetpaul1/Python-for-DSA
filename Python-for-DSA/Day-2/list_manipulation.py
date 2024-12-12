67f0cd0e01b446b6f12c09272c09618b73187709
jeetpaul1 <pauljeet559@gmail.com>
2024-12-12 23:16:46 +0530

    List Manipulation Task done 5/5

---
+++ Python-for-DSA/Day-2/list_manipulation.py
@@ -0,0 +1,16 @@
+#Write a program to create a list of numbers, add an element, remove an element, and calculate its length.
+
+
+numbers = [10, 20, 30, 40] #solution
+
+# Add an element
+numbers.append(50)
+
+# Remove an element
+numbers.remove(20)
+
+# Calculate length
+length = len(numbers)
+
+print("Updated List:", numbers)
+print("Length of List:", length)
