1c9ca383a6e0baaa51659a489fd01f57f5eabea2
jeetpaul1 <pauljeet559@gmail.com>
2024-12-13 23:32:33 +0530

    Day-3

---
+++ Python-for-DSA/Day-3/checking_pos/neg/zero.py
@@ -0,0 +1,12 @@
+#Check If a Number is Positive, Negative, or Zero
+
+
+num = int(input("Enter a number: ")) #solution
+
+# Check conditions
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
