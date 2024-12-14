9e3e8eafbb481dc1cc3d21d29332f40b8f36dcd5
jeetpaul1 <pauljeet559@gmail.com>
2024-12-14 23:45:45 +0530

    square root with build in module

---
+++ Python-for-DSA/Day-4/squareofno.py
@@ -0,0 +1,12 @@
+#Calculate the Square of a Number
+
+
+def square(number):  #solution
+    return number ** 2
+
+# Using the function
+num = 6
+square_result = square(num)
+
+# Print result
+print(f"The square of {num} is {square_result}")
