9847dcc3799c17877cf27eda86feb72045967ed6
jeetpaul1 <pauljeet559@gmail.com>
2024-12-15 23:38:31 +0530

    Counting the Occurrences of an Element in a List

---
+++ Python-for-DSA/Day-5/occurence.py
@@ -0,0 +1,14 @@
+#Write a program to count how many times a specific number appears in a list.
+
+
+
+numbers = [1, 2, 3, 4, 2, 5, 2] #solution
+
+# Input the number to count
+num_to_count = 2
+
+# Count occurrences
+count = numbers.count(num_to_count)
+
+# Print result
+print(f"The number {num_to_count} appears {count} times in the list.")
