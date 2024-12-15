1e3e1618ffa9ff11f22062c72470b2717463248d
jeetpaul1 <pauljeet559@gmail.com>
2024-12-16 00:01:13 +0530

    fixed bug

--- Python-for-DSA/Day-4/square_root.py
+++ Python-for-DSA/Day-4/square_root.py
@@ -1,1 +1,13 @@
 #Use the math module to calculate the square root of a number.
+
+# Import math module
+import math
+
+# Input a number
+number = 16
+
+# Calculate square root
+sqrt_result = math.sqrt(number)
+
+# Print result
+print(f"The square root of {number} is {sqrt_result}")
