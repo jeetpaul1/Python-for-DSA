25f04be3fa43e84862655689522ad060e8c08e2c
jeetpaul1 <pauljeet559@gmail.com>
2024-12-15 23:34:24 +0530

    the Largest and Smallest Numbers in a List

---
+++ Python-for-DSA/Day-5/larg&small_no_list.py
@@ -0,0 +1,12 @@
+#Write a program to find the largest and smallest numbers in a given list.
+
+
+numbers = [3, 7, 2, 8, 5] #solution
+
+# Find largest and smallest
+largest = max(numbers)
+smallest = min(numbers)
+
+# Print results
+print("Largest number:", largest)
+print("Smallest number:", smallest)
