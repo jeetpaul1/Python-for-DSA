c9de12e9ef7b271dfaa9dc213b37e1823ec99e13
jeetpaul1 <pauljeet559@gmail.com>
2024-12-15 23:35:40 +0530

    Reverse a List

---
+++ Python-for-DSA/Day-5/reverse_elements.py
@@ -0,0 +1,11 @@
+#Write a program to reverse the elements of a list.
+
+
+original_list = [1, 2, 3, 4, 5] #solution
+
+# Reverse the list
+reversed_list = original_list[::-1]
+
+# Print results
+print("Original List:", original_list)
+print("Reversed List:", reversed_list)
