826fa1c68c9c02cf203da9f52df674a036deb085
jeetpaul1 <pauljeet559@gmail.com>
2024-12-16 23:28:51 +0530

    Occurrences of an Element

---
+++ Python-for-DSA/Day-6/occurence.py
@@ -0,0 +1,10 @@
+#Count Occurrences of an Element
+
+# Define a tuple
+my_tuple = (1, 2, 3, 2, 4, 2, 5) #solution
+
+# Count occurrences of 2
+count = my_tuple.count(2)
+
+# Print result
+print("Number 2 appears", count, "times in the tuple.")
