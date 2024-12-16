905c82dbf35203c895850681a8cdee77dcf72abe
jeetpaul1 <pauljeet559@gmail.com>
2024-12-16 23:30:57 +0530

    Index of an Element

---
+++ Python-for-DSA/Day-6/index.py
@@ -0,0 +1,11 @@
+#Write a program to find the index of the first occurrence of a specific element in a tuple.
+
+# Define a tuple
+my_tuple = ("apple", "banana", "cherry", "apple") #solution
+
+# Find index of "cherry"
+index = my_tuple.index("cherry")
+
+# Print result
+print("Index of 'cherry':", index)
+
