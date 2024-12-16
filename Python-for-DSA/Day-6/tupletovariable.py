54bcc858860caf678742540e74d77178368070ca
jeetpaul1 <pauljeet559@gmail.com>
2024-12-16 23:34:26 +0530

    Unpack a Tuple into Variables

---
+++ Python-for-DSA/Day-6/tupletovariable.py
@@ -0,0 +1,13 @@
+#Write a program to unpack the elements of a tuple into separate variables.
+
+
+# Define a tuple
+my_tuple = (10, 20, 30) #solution
+
+# Unpack tuple
+a, b, c = my_tuple
+
+# Print unpacked values
+print("a:", a)
+print("b:", b)
+print("c:", c)
