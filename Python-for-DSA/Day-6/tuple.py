875f93c26aa295f6b455f6412c5a118522a03f6a
jeetpaul1 <pauljeet559@gmail.com>
2024-12-16 23:25:53 +0530

    Create and Access a Tuple

---
+++ Python-for-DSA/Day-6/tuple.py
@@ -0,0 +1,11 @@
+#Create and Access a Tuple
+
+# Defining a tuple with mixed data types
+my_tuple = (10, "Python", 3.14, True) #solution
+
+# Access elements using indexing
+print("First element:", my_tuple[0])
+print("Second element:", my_tuple[1])
+
+# Access elements using slicing
+print("Sliced tuple:", my_tuple[1:3])
