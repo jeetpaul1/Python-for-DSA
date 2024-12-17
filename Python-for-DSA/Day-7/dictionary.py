2e36c2f0ea2c3acdaab07ad2bc7ffd90b6a42505
jeetpaul1 <pauljeet559@gmail.com>
2024-12-17 23:42:44 +0530

    Creating and Accessing a Dictionary

---
+++ Python-for-DSA/Day-7/dictionary.py
@@ -0,0 +1,8 @@
+#Write a program to create a dictionary of a student's details and access specific values.
+
+# Creating a dictionary
+student = {"name": "John", "age": 20, "major": "Computer Science"} #solution
+
+# Access values
+print("Name:", student["name"])
+print("Age:", student.get("age"))
---
+++ Python-for-DSA/Day-7/readme.md
@@ -0,0 +1,2 @@
+<h1>Output<h1>
+<img src="/Python-for-DSA/Day-7/" alt="Day 7 Output" width="600">
