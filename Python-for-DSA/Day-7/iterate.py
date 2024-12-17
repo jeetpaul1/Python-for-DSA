18a8a8d55cc93d146ee3e632d8c298940b95da1c
jeetpaul1 <pauljeet559@gmail.com>
2024-12-17 23:47:15 +0530

    Iterate Through a Dictionary

---
+++ Python-for-DSA/Day-7/iterate.py
@@ -0,0 +1,9 @@
+#Write a program to iterate through a dictionary and print all key-value pairs.
+
+
+fruit_prices = {"apple": 100, "banana": 50, "cherry": 200} #solution
+
+# Iterate through the dictionary
+for fruit, price in fruit_prices.items():
+    print(f"The price of {fruit} is {price}")
+ 
