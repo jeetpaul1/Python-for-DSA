85f3973b6f2ce98bd2a6763e4278faa8dd93e60a
jeetpaul1 <pauljeet559@gmail.com>
2024-12-17 23:45:54 +0530

    Add and Update a Dictionary

---
+++ Python-for-DSA/Day-7/update.py
@@ -0,0 +1,13 @@
+#Write a program to add a new key-value pair to a dictionary and update an existing key.
+
+
+student = {"name": "Jeet", "age": 18} #solution
+
+# Add a new key-value pair
+student["major"] = "Python Programming"
+
+# Update an existing key
+student["age"] = 19
+
+# Print updated dictionary
+print("Updated Student Dictionary:", student)
