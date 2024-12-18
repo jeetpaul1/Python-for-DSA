03a96ed14fa23168fd5ba0f58b97f6619a0cdc5b
jeetpaul1 <pauljeet559@gmail.com>
2024-12-18 23:49:05 +0530

    Checking if a Set is a Subset of Another

---
+++ Python-for-DSA/Day-8/subset.py
@@ -0,0 +1,11 @@
+#Write a program to check if one set is a subset of another set.
+
+# Define two sets
+set1 = {1, 2}  #solution
+set2 = {1, 2, 3, 4}
+
+# Check if set1 is a subset of set2
+is_subset = set1.issubset(set2)
+
+# Print result
+print("Is set1 a subset of set2?", is_subset)
