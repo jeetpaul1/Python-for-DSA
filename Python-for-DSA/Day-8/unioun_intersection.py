7c37eb34abc84042eb9ad6f7037b295f141fed2e
jeetpaul1 <pauljeet559@gmail.com>
2024-12-18 23:44:43 +0530

    Finding the Union and Intersection of Two Sets

---
+++ Python-for-DSA/Day-8/unioun_intersection.py
@@ -0,0 +1,13 @@
+#Write a program to find the union and intersection of two sets.
+
+# Define two sets
+set1 = {1, 2, 3} #solution
+set2 = {3, 4, 5}
+
+# Find union and intersection
+union_set = set1.union(set2)
+intersection_set = set1.intersection(set2)
+
+# Print results
+print("Union of sets:", union_set)
+print("Intersection of sets:", intersection_set)
