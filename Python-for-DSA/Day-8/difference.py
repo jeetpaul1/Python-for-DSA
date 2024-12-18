c77b8f0ee211ede890ca06bb65df9f12d76502b0
jeetpaul1 <pauljeet559@gmail.com>
2024-12-18 23:51:05 +0530

    Finding the Difference Between Two Sets

---
+++ Python-for-DSA/Day-8/difference.py
@@ -0,0 +1,11 @@
+#Write a program to find the difference between two sets (elements in one set but not in the other).
+
+# Define two sets
+set1 = {1, 2, 3, 4}  #solution
+set2 = {3, 4, 5, 6}
+
+# Find the difference
+difference_set = set1.difference(set2)
+
+# Print result
+print("Difference between set1 and set2:", difference_set)
