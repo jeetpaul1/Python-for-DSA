c8d8e422098e031db428882920a117b07a810b80
jeetpaul1 <pauljeet559@gmail.com>
2024-12-18 23:39:25 +0530

    Creating a Set and Adding Elements

---
+++ Python-for-DSA/Day-8/sets.py
@@ -0,0 +1,12 @@
+#Write a program to create a set and add multiple elements to it.
+
+# Created an empty set
+my_set = set() #solution
+
+# Add elements to the set
+my_set.add(10)
+my_set.add(20)
+my_set.add(30)
+
+# Print the set
+print("Set after adding elements:", my_set)
